f99c61085aa0fbd23b2379e40f378c5664ae00d9
Minhajh2o <h2o.minhaj@gmail.com>
2024-12-16 23:22:40 +0600

    progress bar size adjusted

--- src/main/java/game/rise_of_valor/Main.java
+++ src/main/java/game/rise_of_valor/Main.java
@@ -12,7 +12,6 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/intro.fxml"));
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/loading.fxml"));
-
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/login-registration.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Ride of Valor");
