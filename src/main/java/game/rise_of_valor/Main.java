075d5b868f734ebcf7f12059bce768516d282dbc
Faysal Ahmed Fahim <ffahim2330717@bscse.uiu.ac.bd>
2025-01-05 11:32:43 +0600

    scene resize problem solved and some transition removed.

--- src/main/java/game/rise_of_valor/Main.java
+++ src/main/java/game/rise_of_valor/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.io.IOException;
 
@@ -11,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/intro.fxml"));
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/introV2.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/intro.fxml"));
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/game-play.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load());
