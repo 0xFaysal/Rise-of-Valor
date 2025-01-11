4c094b6d89cb7cab6d6edb3ed5c95565407e0b21
Faysal Ahmed Fahim <ffahim2330717@bscse.uiu.ac.bd>
2025-01-11 17:07:21 +0600

    lobby start button style complite.

--- src/main/java/game/rise_of_valor/controllers/LobbyTopController.java
+++ src/main/java/game/rise_of_valor/controllers/LobbyTopController.java
@@ -1,17 +1,31 @@
 package game.rise_of_valor.controllers;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LobbyTopController {
+public class LobbyTopController  implements Initializable{
 
     int n=1;
+
+    @FXML
+    private Button playBtn;
 
     @FXML
     private AnchorPane rootPane;
@@ -23,10 +37,10 @@ public class LobbyTopController {
         LobbyViewController lobbyViewController = (LobbyViewController) stage.getScene().getUserData();
         lobbyViewController.print();
     }
+
     public void playNowBtnClick(ActionEvent actionEvent) {
         try {
             Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-
 
             // Stop the lobby loop
             LobbyViewController lobbyViewController = (LobbyViewController) stage.getScene().getUserData();
@@ -37,11 +51,14 @@ public class LobbyTopController {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/game/rise_of_valor/fxml/game-play.fxml"));
             stage.getScene().setRoot(loader.load());
 
-
-
-
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
     }
 }
