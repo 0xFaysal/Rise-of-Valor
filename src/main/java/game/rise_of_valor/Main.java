c59fe1acb131512ddaaf6a9ba69a4f88b00b6987
Minhajh2o <h2o.minhaj@gmail.com>
2025-01-15 19:15:24 +0600

    mode selection controller function added successfully

--- src/main/java/game/rise_of_valor/Main.java
+++ src/main/java/game/rise_of_valor/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/mode-selection.fxml"));
-        //FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/intro.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/intro.fxml"));
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/loading.fxml"));
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/login-registration.fxml"));
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/lobby-view.fxml"));
--- src/main/java/game/rise_of_valor/controllers/ModeSelectionController.java
+++ src/main/java/game/rise_of_valor/controllers/ModeSelectionController.java
@@ -1,4 +1,14 @@
 package game.rise_of_valor.controllers;
 
+import javafx.fxml.FXML;
+
 public class ModeSelectionController {
+    @FXML
+    public void duoModeClicked() {
+        System.out.println("Game started in duo mode");
+    }
+    @FXML
+    public void quadModeClicked() {
+        System.out.println("Game started in quad mode");
+    }
 }
