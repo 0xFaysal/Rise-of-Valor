e6c5af762912dc3ec53bf0379c0442abdc79ac91
Minhajh2o <h2o.minhaj@gmail.com>
2025-01-20 21:10:00 +0600

    shop inside style added

--- src/main/java/game/rise_of_valor/Main.java
+++ src/main/java/game/rise_of_valor/Main.java
@@ -3,6 +3,7 @@ package game.rise_of_valor;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,13 +11,14 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/intro.fxml"));
-//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/loading.fxml"));
-//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/l ogin-registration.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/intro.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/loading.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/login-registration.fxml"));
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/lobby-view.fxml"));
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/game-play.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Ride of Valor");
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("assets/image/gameLogoLoginPage.png")));
         stage.setScene(scene);
         stage.show();
     }
